f5203544d18c21f3a425d02c40036632b6e00241
Ivan Sirotyuk <ivan.sirotyuk@gmail.com>
2013-09-12 20:21:38 +0300

    merge with maket

---
+++ maket/map.js
@@ -0,0 +1,39 @@
+var map = null;
+var points = new Array();
+
+// Create event handler window.onLoad
+YMaps.jQuery(function () {
+	map = new YMaps.Map(YMaps.jQuery("#map")[0]);
+	map.setCenter(new YMaps.GeoPoint(25.311073, 50.744059), 10);
+	map.addControl(new YMaps.Zoom());
+});
+
+//Add point to the map
+function addPoint(lng, lat, name, desc){
+	//create placemark and set property
+	var placemark = new YMaps.Placemark(new YMaps.GeoPoint(lng, lat));
+	placemark.name = name;
+	placemark.description = desc;
+	placemark.setIconContent("place");
+	placemark.id = points.length;
+
+	//set  callback function to placemark
+	YMaps.Events.observe(placemark, placemark.Events.Click, placemarkCallback);
+
+	//save and display placemark
+	points.push(placemark);
+	map.addOverlay(placemark); 
+}
+
+//Placemark callback function
+function placemarkCallback(placemark, e){
+	alert(placemark.id);
+}
+
+function add(){
+	var lng = document.getElementById("lng").value;
+	var lat = document.getElementById("lat").value;
+	var name = document.getElementById("name").value;
+	var desc = document.getElementById("desc").value;
+	addPoint(lng, lat, name, desc);
+}
