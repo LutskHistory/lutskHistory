818d6f31cd67e8a60706d7368008cbba7f009f80
Ivan Sirotyuk <ivan.sirotyuk@gmail.com>
2013-09-14 19:53:09 +0300

    work with yandex map

--- maket/map.js
+++ maket/map.js
@@ -1,5 +1,6 @@
 var map = null;
 var points = new Array();
+var layerPointCache = Array();
 
 // Create event handler window.onLoad
 YMaps.jQuery(function () {
@@ -9,13 +10,14 @@ YMaps.jQuery(function () {
 });
 
 //Add point to the map
-function addPoint(lng, lat, name, desc){
+function addPoint(id, lng, lat, name, desc, layerID){
 	//create placemark and set property
-	var placemark = new YMaps.Placemark(new YMaps.GeoPoint(lng, lat));
+	var placemark = new YMaps.Placemark(new YMaps.GeoPoint(lat, lng));
 	placemark.name = name;
 	placemark.description = desc;
 	placemark.setIconContent("place");
-	placemark.id = points.length;
+	placemark.id = id;
+	placemark.layerID = layerID;
 
 	//set  callback function to placemark
 	YMaps.Events.observe(placemark, placemark.Events.Click, placemarkCallback);
@@ -33,8 +35,15 @@ function placemarkCallback(placemark, e){
         type: "get",
         data: data,
         success: function(data){
-            alert(data);
-            
+        	posts = JSON.parse(data);
+        	for (i = 0; i < posts.length; i++){
+        		var post = "";
+        		for (var field in posts[i]){
+        			post += field + " - ";
+        		}
+        		$("div#content").append("<p>" + post + "</p>");
+        		alert(post);
+        	}
         },
         error:function(){
             alert("failure");
@@ -49,4 +58,70 @@ function add(){
 	var name = document.getElementById("name").value;
 	var desc = document.getElementById("desc").value;
 	addPoint(lng, lat, name, desc);
+}
+
+//Get map layer
+function getLayers(){
+	$.ajax({
+        url: "http://lutskhistory.dev/index.php/layers/get",
+        type: "get",
+        data: "",
+        success: function(data){
+        	layers = JSON.parse(data);
+        	for (i = 0; i < layers.length; i++){
+        		layer = '<input type="checkbox" id="' + layers[i].id + '" class="layerCheckBox" onClick="getLayerPoints(' + layers[i].id + ')">'+ layers[i].name + '<br />';
+        		$('div#map-control-panel').append(layer);
+        	}
+        },
+        error:function(){
+            alert("failure");
+
+        }
+    });
+
+}
+
+//Get points of layer
+function getLayerPoints(layerID){
+	if ($('input.layerCheckBox#' + layerID).attr('checked')){
+
+		for (i = 0; i < layerPointCache.length; i++){
+			if (layerPointCache[i] == layerID){
+				for(var id in points){
+					if (points[id].layerID == layerID)
+						map.addOverlay(points[id]);
+				}
+				return;
+			}
+		}
+
+
+		var data = {id : layerID};
+		$.ajax({
+	        url: "http://lutskhistory.dev/index.php/layers/getPoints",
+	        type: "get",
+	        data: data,
+	        success: function(data){
+	        	mapPoints = JSON.parse(data);
+	        	for (i = 0; i < mapPoints.length; i++){
+	        		addPoint(mapPoints[i].id, mapPoints[i].lng, mapPoints[i].lat, mapPoints[i].name, mapPoints[i].description, layerID);
+	        	}
+	        	layerPointCache.push(layerID);
+	        },
+
+	        error:function(data, data1, data2){
+	            alert("failure");
+	        }
+	    });
+	}
+
+	else{
+		for (i = 0; i < points.length; i++) {
+			if (points[i].layerID == layerID){
+				map.removeOverlay(points[i]);
+			}
+		};
+
+	}
+
 }
